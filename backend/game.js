848c555eda9b72accfea9c93463499164947484d
umutpiri <umutpc97@gmail.com>
2019-12-08 21:17:22 +0300

    socket initialize

--- backend/game.js
+++ backend/game.js
@@ -1,10 +1,14 @@
 var socketio = require("socket.io");
 
+var players = [];
+
 
 module.exports.listen = function(app) {
     io = socketio.listen(app);
+    
     io.on("connection", function(socket){
         console.log("connected");
+        players.push({name: "test", socket: socket});
 
         socket.on("disconnect", function(){
             console.log("disconnected");
--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,6 @@ app.get("/", function(req, res) {
     console.log("hello");
 });
 
-app.get("*", function(req, res){
-    res.status(404).send("Page not found");
-});
-
 http.listen(app.get("port"), function(){
     console.log("App started on port %s", app.get("port"));
 })
